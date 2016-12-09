7e8190501fad29c72d96b0fa9b2dcd9b1fba657d
Phoenix-616 <ksandr007@gmail.com>
2016-12-09 12:13:24 +0800

    Forwarder, now with buffers and postsend

--- forward.c
+++ forward.c
@@ -21,7 +21,10 @@ int main(int argc, char *argv[])
 	int listen_fd, server_fd, client_fd;
 	struct sockaddr_in server_addr, forward_addr;
 	struct hostent * he = NULL;
-	char buf[BUF_SIZE];
+	char cl_buf[BUF_SIZE];
+	char ser_buf[BUF_SIZE];
+	int cl_read = 0, cl_got = 0;
+	int ser_read = 0, ser_got = 0;
 	int ret;
 	fd_set sel_set, sel_set2;
 	struct timeval sel_time;
@@ -194,7 +197,7 @@ int main(int argc, char *argv[])
 			transfer = 1;
 			fprintf(stderr, "Connection established\n");
 		}
-		
+		/*
 		if ((ret > 0) && (transfer))
 		{
 			if (FD_ISSET(client_fd, &sel_set) && FD_ISSET(server_fd, &sel_set2))
@@ -245,6 +248,91 @@ int main(int argc, char *argv[])
 						{
 							perror("Send to client failed");
 						}
+					}
+				}
+			}
+		}
+		*/
+		
+		if ((ret > 0) && (transfer))
+		{
+			if ((FD_ISSET(server_fd, &sel_set)) && (cl_got < BUF_SIZE))
+			{
+				ret = recv(server_fd, (cl_buf + cl_got), (BUF_SIZE - cl_got), 0);
+				if (ret < 0)
+				{
+					if (!((errno == EAGAIN) || (errno == EWOULDBLOCK)))
+						perror("recv from server failed");
+				}
+				else
+				{
+					if (ret == 0)
+					{
+						fprintf(stderr, "Server closed connection\n");
+						exit(0);
+					}
+					cl_got += ret;
+				}
+			}
+			if ((FD_ISSET(client_fd, &sel_set)) && (ser_got < BUF_SIZE))
+			{
+				ret = recv(client_fd, (ser_buf + ser_got), (BUF_SIZE - ser_got), 0);
+				if (ret < 0)
+				{
+					if (!((errno == EAGAIN) || (errno == EWOULDBLOCK)))
+						perror("recv from client failed");
+				}
+				else
+				{
+					if (ret == 0)
+					{
+						fprintf(stderr, "Client closed connection\n");
+						exit(0);
+					}
+					ser_got += ret;
+				}
+			}
+			if ((FD_ISSET(server_fd, &sel_set2)) && (ser_read < ser_got))
+			{
+				ret = send(server_fd, (ser_buf + ser_read), (ser_got - ser_read), 0);
+				if (ret < 0)
+				{
+					if (!((errno == EAGAIN) || (errno == EWOULDBLOCK)))
+						perror("send to server failed");
+				}
+				else
+				{
+					if (ret == 0)
+					{
+						fprintf(stderr, "Nothing sended to server\n");
+					}
+					ser_read += ret;
+					if (ser_read == ser_got)
+					{
+						ser_read = 0;
+						ser_got = 0;
+					}
+				}
+			}
+			if ((FD_ISSET(client_fd, &sel_set2)) && (cl_read < cl_got))
+			{
+				ret = send(client_fd, (cl_buf + cl_read), (cl_got - cl_read), 0);
+				if (ret < 0)
+				{
+					if (!((errno == EAGAIN) || (errno == EWOULDBLOCK)))
+						perror("send to client failed");
+				}
+				else
+				{
+					if (ret == 0)
+					{
+						fprintf(stderr, "Nothing sended to client\n");
+					}
+					cl_read += ret;
+					if (cl_read == cl_got)
+					{
+						cl_read = 0;
+						cl_got = 0;
 					}
 				}
 			}
